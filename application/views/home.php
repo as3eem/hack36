7cea8c6063b9773fb915f54164c22daaaaf07a28
kpiyushit <krpiyush5@gmail.com>
2018-01-27 17:08:30 +0530

    jiskodekhnahdekhle

--- application/views/home.php
+++ application/views/home.php
@@ -9,38 +9,63 @@
 		<title>Story by HTML5 UP</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<link rel="stylesheet" href="<?=base_url()?>home/assets/css/main.css" />
+		 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="assets/css/main.css" />
 	</head>
 	<body>
-
+     
 		<!-- Wrapper -->
 			<div id="wrapper" class="divided">
 
 				<!-- One -->
 					<section class="banner style1 orient-left content-align-left image-position-right fullscreen onload-image-fade-in onload-content-fade-right">
 						<div class="content">
-							<h1>Story</h1>
-							<p class="major">A (modular, highly tweakable) responsive one-page template designed by <a href="https://github.com/as3eem">HTML5 UP</a> and released for free under the <a href="https://github.com/kpiyushit">Creative Commons</a>.</p>
+						<h1>CodeChamps</h1>
+							<h3 id='random_quote'>Story</h3>
+							<!-- <p class="major">A (modular, highly tweakable) responsive one-page template designed by <a href="https://html5up.net">HTML5 UP</a> and released for free under the <a href="https://html5up.net/license">Creative Commons</a>.</p> -->
 							<ul class="actions vertical">
-								<li><input type="submit" value="Register" class="button big wide smooth-scroll-middle">
+								<li><a href="#first" class="button big wide smooth-scroll-middle">Register</a></li>
+							</ul>
 							<ul class="actions vertical">
 								<li><a href="#first" class="button big wide smooth-scroll-middle">Get Started</a></li>
 							</ul>
 						</div>
 						<div class="image">
-							<img src="<?=base_url()?>home/images/banner.jpg" alt="" />
+							<img src="images/banner.jpg" alt="" />
 						</div>
 					</section>
 
 				
 
 		 Scripts -->
-			<script src="<?=base_url()?>home/assets/js/jquery.min.js"></script>
-			<script src="<?=base_url()?>home/assets/js/jquery.scrollex.min.js"></script>
-			<script src="<?=base_url()?>home/assets/js/jquery.scrolly.min.js"></script>
-			<script src="<?=base_url()?>home/assets/js/skel.min.js"></script>
-			<script src="<?=base_url()?>home/assets/js/util.js"></script>
-			<script src="<?=base_url()?>home/assets/js/main.js"></script>
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+            <script>
+             function randomQuote() {
+        $.ajax({
+            url: "https://api.forismatic.com/api/1.0/?",
+            dataType: "jsonp",
+            data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+            success: function( response ) {
+                $("#random_quote").html("<p id='random_quote' class='lead text-center'>" +
+                    response.quoteText + "<br/>&dash; " + response.quoteAuthor + " &dash;</p>");
+                $("#tweet").attr("href", "https://twitter.com/home/?status=" + response.quoteText +
+                    ' (' + response.quoteAuthor + ')');
+            }
+        });
+    }
 
+    $(function() {
+        randomQuote();
+    });
+
+    $("button").click(function(){
+        randomQuote();
+    });
+            </script>
 	</body>
 </html>
