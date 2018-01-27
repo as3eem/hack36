444372a77fa8392e32de1894b41e5ecbdda32a90
kpiyushit <krpiyush5@gmail.com>
2018-01-27 14:52:34 +0530

    mdskn

---
+++ hack36.sql
@@ -0,0 +1,15 @@
+
+CREATE DATABASE IF NOT EXISTS`hack36`;
+USE hack36;
+
+DROP TABLE IF EXISTS `userdata`;
+CREATE TABLE IF NOT EXISTS `userdata` (
+  	`id` bigint(20) NOT NULL AUTO_INCREMENT,
+	`NAME` varchar(100),
+   	`EMAIL` varchar(120),
+   	`CONTACT` varchar(100),
+   	`STREET` varchar(125),
+   	`CITY` varchar(1000),
+    	`PROFESSION` varchar(125),
+  PRIMARY KEY (id)
+) ;
