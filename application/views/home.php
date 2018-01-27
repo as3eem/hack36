519bfaee475b739c50b462d4cee0e3fc16c08a34
kpiyushit <krpiyush5@gmail.com>
2018-01-27 17:31:25 +0530

    fixed random quote bug

--- application/views/home.php
+++ application/views/home.php
@@ -9,44 +9,43 @@
 		<title>Story by HTML5 UP</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		<link rel="stylesheet" href="assets/css/main.css" />
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="<?=base_url()?>home/assets/css/main.css" />
 	</head>
 	<body>
-     
+
 		<!-- Wrapper -->
 			<div id="wrapper" class="divided">
 
 				<!-- One -->
 					<section class="banner style1 orient-left content-align-left image-position-right fullscreen onload-image-fade-in onload-content-fade-right">
 						<div class="content">
-						<h1>CodeChamps</h1>
-							<h3 id='random_quote'>Story</h3>
-							<!-- <p class="major">A (modular, highly tweakable) responsive one-page template designed by <a href="https://html5up.net">HTML5 UP</a> and released for free under the <a href="https://html5up.net/license">Creative Commons</a>.</p> -->
+							<h3 id ='random_quote'>Story</h3>
+							<!-- <p class="major">A (modular, highly tweakable) responsive one-page template designed by <a href="https://github.com/as3eem">HTML5 UP</a> and released for free under the <a href="https://github.com/kpiyushit">Creative Commons</a>.</p> -->
 							<ul class="actions vertical">
-								<li><a href="#first" class="button big wide smooth-scroll-middle">Register</a></li>
-							</ul>
+								<li><input type="submit" value="Register" class="button big wide smooth-scroll-middle">
 							<ul class="actions vertical">
 								<li><a href="#first" class="button big wide smooth-scroll-middle">Get Started</a></li>
 							</ul>
 						</div>
 						<div class="image">
-							<img src="images/banner.jpg" alt="" />
+							<img src="<?=base_url()?>home/images/banner.jpg" alt="" />
 						</div>
 					</section>
 
 				
 
-		 Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.scrollex.min.js"></script>
-			<script src="assets/js/jquery.scrolly.min.js"></script>
-			<script src="assets/js/skel.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
-            <script>
-             function randomQuote() {
-        $.ajax({
+		 <!-- Scripts --> 
+			<script src="<?=base_url()?>home/assets/js/jquery.min.js"></script>
+			<script src="<?=base_url()?>home/assets/js/jquery.scrollex.min.js"></script>
+			<script src="<?=base_url()?>home/assets/js/jquery.scrolly.min.js"></script>
+			<script src="<?=base_url()?>home/assets/js/skel.min.js"></script>
+			<script src="<?=base_url()?>home/assets/js/util.js"></script>
+			<!-- <script src="<?=base_url()?>home/assets/js/main.js"></script> -->
+
+			<script type="text/javascript">
+			function randomQuote() {
+        	$.ajax({
             url: "https://api.forismatic.com/api/1.0/?",
             dataType: "jsonp",
             data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
@@ -66,6 +65,6 @@
     $("button").click(function(){
         randomQuote();
     });
-            </script>
+			</script>
 	</body>
 </html>
