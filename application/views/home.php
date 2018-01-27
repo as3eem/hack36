4ea75729da984a1606197258ce28eb2befeed36c
Aseem Srivastava <as3eem@gmail.com>
2018-01-27 16:59:14 +0530

    front end improved

---
+++ application/views/home.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML>
+<!--
+	Story by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Story by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="<?=base_url()?>home/assets/css/main.css" />
+	</head>
+	<body>
+
+		<!-- Wrapper -->
+			<div id="wrapper" class="divided">
+
+				<!-- One -->
+					<section class="banner style1 orient-left content-align-left image-position-right fullscreen onload-image-fade-in onload-content-fade-right">
+						<div class="content">
+							<h1>Story</h1>
+							<p class="major">A (modular, highly tweakable) responsive one-page template designed by <a href="https://github.com/as3eem">HTML5 UP</a> and released for free under the <a href="https://github.com/kpiyushit">Creative Commons</a>.</p>
+							<ul class="actions vertical">
+								<li><input type="submit" value="Register" class="button big wide smooth-scroll-middle">
+							<ul class="actions vertical">
+								<li><a href="#first" class="button big wide smooth-scroll-middle">Get Started</a></li>
+							</ul>
+						</div>
+						<div class="image">
+							<img src="<?=base_url()?>home/images/banner.jpg" alt="" />
+						</div>
+					</section>
+
+				
+
+		 Scripts -->
+			<script src="<?=base_url()?>home/assets/js/jquery.min.js"></script>
+			<script src="<?=base_url()?>home/assets/js/jquery.scrollex.min.js"></script>
+			<script src="<?=base_url()?>home/assets/js/jquery.scrolly.min.js"></script>
+			<script src="<?=base_url()?>home/assets/js/skel.min.js"></script>
+			<script src="<?=base_url()?>home/assets/js/util.js"></script>
+			<script src="<?=base_url()?>home/assets/js/main.js"></script>
+
+	</body>
+</html>
--- hack36.sql
+++ hack36.sql
@@ -2,14 +2,14 @@
 CREATE DATABASE IF NOT EXISTS`hack36`;
 USE hack36;
 
-DROP TABLE IF EXISTS `userdata`;
-CREATE TABLE IF NOT EXISTS `userdata` (
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
   	`id` bigint(20) NOT NULL AUTO_INCREMENT,
-	`NAME` varchar(100),
+	  `NAME` varchar(100),
    	`EMAIL` varchar(120),
    	`CONTACT` varchar(100),
    	`STREET` varchar(125),
    	`CITY` varchar(1000),
-    	`PROFESSION` varchar(125),
+   	`PROFESSION` varchar(125),
   PRIMARY KEY (id)
 ) ;
